49fabd9d693feff2d129676c89e3a95b56db2b4d
Gus <gustavoocanto@gmail.com>
2021-11-03 16:49:48 +0800

    start workin on the template

---
+++ src/components/Layout.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Header from './Header.vue'
+	import { Header as HeaderInfo } from './Types'
+	import { defineComponent } from 'vue';
+
+	const HeaderInfo = <HeaderInfo>{
+		name: 'Gustavo Ocanto',
+		email: 'info@gocanto.dev',
+		phoneNumber: '+65 8292 9269',
+	}
+
+	export default defineComponent({
+		components: {
+			Header,
+		},
+		setup() {
+			return {
+				HeaderInfo,
+			}
+		},
+	})
+</script>
+
+<template>
+	<Header :header="HeaderInfo"></Header>
+</template>
---
+++ src/components/Types.ts
@@ -0,0 +1,5 @@
+export interface Header {
+  name: string,
+  email: string,
+  phoneNumber: string,
+}
