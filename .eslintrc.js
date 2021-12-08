f768ed639317534a3f9ab120dce42e5d02dda472
Gus <gustavoocanto@gmail.com>
2021-12-08 23:01:14 +0800

    fix eslint config for Vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+    root: true,
     env: {
         node: true,
         'vue/setup-compiler-macros': true,
@@ -6,12 +7,20 @@ module.exports = {
     extends: [
         "eslint:recommended",
         "plugin:vue/vue3-recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
         "prettier",
     ],
     plugins: [
-        "vue"
+        "vue",
+        "@typescript-eslint"
     ],
     rules: {
         'vue/script-setup-uses-vars': 'error',
     },
+    parser: "vue-eslint-parser",
+    parserOptions: {
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    }
 }
