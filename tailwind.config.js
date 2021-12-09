3d8e26a7dc9d0de077d36ed8248e1cd84ab6cd23
Gus <gustavoocanto@gmail.com>
2021-12-09 11:54:47 +0800

    trim the TW config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,13 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        teal: colors.teal,
-        cyan: colors.cyan,
-        rose: colors.rose,
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [
       require('@tailwindcss/typography'),
-      require('@tailwindcss/aspect-ratio'),
   ],
 }
