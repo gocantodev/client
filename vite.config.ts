bfb9f715f707d7c18da093beeaeb1e8d5b501c09
Gus <gustavoocanto@gmail.com>
2021-12-09 11:05:43 +0800

    tweak the TS config file + updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     resolve:{
-        alias:{
-            '@' : path.resolve(__dirname, './src')
-        },
+        alias:[
+            { find: '@', replacement: '/src' },
+            { find: '@component', replacement: '/src/component' },
+            { find: '@entity', replacement: '/src/entity' }
+        ],
     },
     plugins: [vue()]
 })
